c3b5f524846d44d2a4f1e47826f822e84786c201
claudiavalencia <claudia.valencia@sofka.com.co>
2022-02-21 15:03:51 -0500

    se crea el escenario inicio de sesion

---
+++ out/test/resources/features/iniciarsesion/iniciarsesion.feature
@@ -0,0 +1,21 @@
+# new feature
+# Tags: optional
+
+Feature: Inicio de sesion
+  Como cliente de Hibot
+  necesito inicar sesion
+  para configurar mis canales
+
+  Background:
+    Given que el cliente está en la página principal de hibot
+
+  @Scenario1
+  Scenario Outline: inicio de sesion valido
+    When el cliente navegue hasta el boton iniciar sesión y llene los siguentes datos
+    |correo|<correo>|
+    |contaseña|<contrasena>|
+    Then la pagina le debe mostrar las novedades de la app
+
+Examples:
+  |correo|contrasena|
+  |claudia.valencia.qa@sofka.com.co|Pruebas2022@|
---
+++ src/test/java/qa/hibot/stepdefinitions/iniciarsesion/iniciarsesionstepdefinition.java
@@ -0,0 +1,25 @@
+package qa.hibot.stepdefinitions.iniciarsesion;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class iniciarsesionstepdefinition {
+
+    //Background:
+    @Given("que el cliente está en la página principal de hibot")
+    public void que_el_cliente_esta_en_la_pagina_principal_de_hibot() {
+
+    }
+    //Scenario1
+    @When("el cliente navegue hasta el boton iniciar sesión y llene los siguentes datos")
+    public void el_cliente_navegue_hasta_el_boton_iniciar_sesión_y_llene_los_siguentes_datos(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+
+    @Then("la pagina le debe mostrar las novedades de la app")
+    public void la_pagina_le_debe_mostrar_las_novedades_de_la_app() {
+
+    }
+
+}
